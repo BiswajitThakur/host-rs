a5e1bedc48c9d078e5caba0a780a89ffd237761a
BiswajitThakur <biswajit733127@gmail.com>
2024-02-09 22:45:36 +0530

    _

--- README.md
+++ README.md
@@ -8,6 +8,22 @@ Block Website, Ads, Social, Porn, Fakenews, Gambling, Spam sites and website red
 > [!WARNING]
 > Work in progress
 
+## Installation
+
+### Install from source code on Linux
+```bash
+git clone https://github.com/BiswajitThakur/host-rs
+cd host-rs
+./setup.sh
+make build
+sudo make install
+```
+### Install from precompiled binary
+1. [Click here to Download](https://github.com/BiswajitThakur/host-rs/releases) - download sutable binary for your system
+1. `cd ~/Downloads/` - Go to the location of the binarry file
+1. `chmod +x installer_host-rs_*.sh` - Add executable permission
+1. `sudo ./installer_host-rs_*.sh` - Execute
+
 ## Features of Host-RS
 - [x] Block websites
 - [x] Unblock websites
@@ -22,3 +38,31 @@ Block Website, Ads, Social, Porn, Fakenews, Gambling, Spam sites and website red
 - [x] Unlock social websites
 - [x] Block gambling websites
 - [x] Unblock gambling websites
+
+## Help
+```
+> host-rs --help
+Usage: host-rs [OPTIONS]
+
+Options:
+  -b, --block-web <block-web>...                    Block websites.
+  -u, --unblock-web <unblock-web>...                Unblock websites.
+      --add-redirect <add-redirect> <add-redirect>  Redirect website
+      --rm-redirect <rm-redirect>...                remove from redirect
+      --block-ads                                   Block Ads.
+      --unblock-ads                                 Unblock all ads.
+      --block-porn                                  Block all porn sites.
+      --unblock-porn                                Unblock porn websites.
+      --block-fakenews                              Block fakenews websites.
+      --unblock-fakenews                            Unblock fakenews websites.
+      --block-social                                Block all social websites.
+      --unblock-social                              Unlock social websites.
+      --block-gambling                              Block gambling websites.
+      --unblock-gambling                            Unblock gambling websites.
+      --update-self                                 Update if available.
+      --remove-self                                 Uninstall
+      --update-sources                              Update host sources if available.
+  -h, --help                                        Print help
+  -V, --version                                     Print version
+
+```
--- src/cli/options.rs
+++ src/cli/options.rs
@@ -6,6 +6,7 @@ pub fn init() -> clap::ArgMatches {
             Arg::new("block-web")
                 .short('b')
                 .long("block-web")
+                .aliases(["bw", "BW"])
                 .help("Block websites.")
                 .num_args(1..)
                 .action(ArgAction::Append),
@@ -14,20 +15,40 @@ pub fn init() -> clap::ArgMatches {
             Arg::new("unblock-web")
                 .short('u')
                 .long("unblock-web")
+                .aliases(["uw", "UW"])
                 .help("Unblock websites.")
                 .conflicts_with("block-web")
                 .num_args(1..)
                 .action(ArgAction::Append),
         )
         .arg(
+            Arg::new("add-redirect")
+                .long("add-redirect")
+                .aliases(["ar", "AR"])
+                .help("Redirect website")
+                .num_args(2)
+                .action(ArgAction::Append),
+        )
+        .arg(
+            Arg::new("rm-redirect")
+                .long("rm-redirect")
+                .aliases(["rr", "RR"])
+                .help("remove from redirect")
+                .conflicts_with("add-redirect")
+                .num_args(1..)
+                .action(ArgAction::Append),
+        )
+        .arg(
             Arg::new("block-ads")
                 .long("block-ads")
+                .aliases(["ba", "BA"])
                 .help("Block Ads.")
                 .action(ArgAction::SetTrue),
         )
         .arg(
             Arg::new("unblock-ads")
                 .long("unblock-ads")
+                .aliases(["ua", "UA", "uba"])
                 .help("Unblock all ads.")
                 .conflicts_with("block-ads")
                 .action(ArgAction::SetTrue),
@@ -35,12 +56,14 @@ pub fn init() -> clap::ArgMatches {
         .arg(
             Arg::new("block-porn")
                 .long("block-porn")
+                .aliases(["bp", "BP"])
                 .help("Block all porn sites.")
                 .action(ArgAction::SetTrue),
         )
         .arg(
             Arg::new("unblock-porn")
                 .long("unblock-porn")
+                .aliases(["up", "UP", "ubp"])
                 .help("Unblock porn websites.")
                 .conflicts_with("block-porn")
                 .action(ArgAction::SetTrue),
@@ -48,12 +71,14 @@ pub fn init() -> clap::ArgMatches {
         .arg(
             Arg::new("block-fakenews")
                 .long("block-fakenews")
+                .aliases(["bf", "BF"])
                 .help("Block fakenews websites.")
                 .action(ArgAction::SetTrue),
         )
         .arg(
             Arg::new("unblock-fakenews")
                 .long("unblock-fakenews")
+                .aliases(["uf", "UF", "ubf"])
                 .help("Unblock fakenews websites.")
                 .conflicts_with("block-fakenews")
                 .action(ArgAction::SetTrue),
@@ -61,12 +86,14 @@ pub fn init() -> clap::ArgMatches {
         .arg(
             Arg::new("block-social")
                 .long("block-social")
+                .aliases(["bs", "BS"])
                 .help("Block all social websites.")
                 .action(ArgAction::SetTrue),
         )
         .arg(
             Arg::new("unblock-social")
                 .long("unblock-social")
+                .aliases(["us", "US", "ubs"])
                 .help("Unlock social websites.")
                 .conflicts_with("block-social")
                 .action(ArgAction::SetTrue),
@@ -74,12 +101,14 @@ pub fn init() -> clap::ArgMatches {
         .arg(
             Arg::new("block-gambling")
                 .long("block-gambling")
+                .aliases(["bg", "BG"])
                 .help("Block gambling websites.")
                 .action(ArgAction::SetTrue),
         )
         .arg(
             Arg::new("unblock-gambling")
                 .long("unblock-gambling")
+                .aliases(["ug", "UG", "ubg"])
                 .help("Unblock gambling websites.")
                 .conflicts_with("block-gambling")
                 .action(ArgAction::SetTrue),
@@ -88,6 +117,13 @@ pub fn init() -> clap::ArgMatches {
             Arg::new("update-self")
                 .long("update-self")
                 .help("Update if available.")
+                .action(ArgAction::SetTrue),
+        )
+        .arg(
+            Arg::new("rm-self")
+                .long("remove-self")
+                .alias("rm-self")
+                .help("Uninstall")
                 .action(ArgAction::SetTrue),
         )
         .arg(
