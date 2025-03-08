e7a4d5bb125f4d78b0a9a3a1b3adfe5d89d75a81
BiswajitThakur <biswajit733127@gmail.com>
2025-03-08 10:34:02 +0530

    update dependency & bug fix

--- crates/host-rs/src/main.rs
+++ crates/host-rs/src/main.rs
@@ -1,7 +1,14 @@
+use std::env::args;
+
 mod cli;
 
 fn main() {
+    if args().collect::<Vec<_>>().len() < 2 {
+        println!("Usage: {} [OPTIONS] [COMMAND]", env!("CARGO_PKG_NAME"));
+        std::process::exit(1);
+    }
     if let Err(err) = cli::run() {
         eprintln!("{}", err);
+        std::process::exit(1);
     }
 }
